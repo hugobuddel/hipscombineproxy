7c620c614c90aa52d9a6ec76ecb93f8c11d9d79a
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2019-07-24 12:27:10 +0200

    Add some documentation

---
+++ README.rst
@@ -0,0 +1,7 @@
+HiPS Combine Proxy
+==================
+
+A proxy that automatically combines tiles from HiPS servers.
+
+`createpoc.sh` starts an proof of concept.
+
--- createpoc.sh
+++ createpoc.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+#####
+# Get Aladin.
+#####
+
 mkdir poctools
 pushd poctools
 wget http://aladin.unistra.fr/java/download/AladinBeta.jar
 popd
 
+
+#####
+# Setup Python environment.
+#####
+
+python -m venv pocvenv
+source pocvenv/bin/activate
+pip install requests pillow
+
+
+#####
+# For two overlapping FITS files:
+# * Fetch the FITS files,
+# * Convert them to HiPS tiles,
+# * Start the webserver.
+#####
 
 mkdir pocdata
 pushd pocdata
@@ -13,37 +33,32 @@ mkdir fits1
 wget -P fits1 http://ds.astro.rug.astro-wise.org:8000/Sci-JMCFARLAND-OMEGACAM-------OCAM_r_SDSS-ESO_CCD_%2366-Regr---Sci-56981.2866678-892f93277ec0f44eaaec225e48c2d8a370dd5e7e.fits
 mkdir hips1
 java -Xmx16g -jar ../poctools/AladinBeta.jar -hipsgen in=fits1 out=hips1 creator_did=HiPSID INDEX TILES PNG DETAILS CLEANFITS
-
+python -m http.server 8101 --directory hips1 &
 
 mkdir fits2
 wget -P fits2 http://ds.astro.rug.astro-wise.org:8000/Sci-JMCFARLAND-OMEGACAM-------OCAM_r_SDSS-ESO_CCD_%2366-Regr---Sci-56981.2869923-3c338b3af3e4b211ff86e323ad4b4ac39507a1d9.fits
 mkdir hips2
 java -Xmx16g -jar ../poctools/AladinBeta.jar -hipsgen in=fits2 out=hips2 creator_did=HiPSID INDEX TILES PNG DETAILS CLEANFITS
+python -m http.server 8102 --directory hips2 &
 
-mkdir hipsauto
-cp -avi hips1/properties hipsauto/properties
 popd
 
 
-python -m venv pocvenv
-source pocvenv/bin/activate
-pip install requests pillow
+#####
+# Setup and start the HiPS Proxy
+#####
 
-
-pushd pocdata/hips1
-python -m http.server 8101 &
-popd
-
-pushd pocdata/hips2
-python -m http.server 8102 &
-popd
-
+mkdir pocdata/hipsauto
+cp -avi pocdata/hips1/properties pocdata/hipsauto/properties
 pushd pocdata/hipsauto
 python ../../hipscombineproxy.py &
 popd
 
+
+#####
+# Start Aladin
+#####
+
 java -jar poctools/AladinBeta.jar "http://localhost:8100"
 
-
-
-
+# Aladin should now start and look like aladinpoc.png
